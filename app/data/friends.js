7b1b7bdbfb22a95ed237db121325a1f7051641d8
Alex Curington <alex.curington@me.com>
2019-03-15 19:54:40 -0600

    no message

---
+++ app/data/friends.js
@@ -0,0 +1,323 @@
+var friends = [
+  {
+  "name": "Ahmed",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+  "scores": [
+  "5",
+  "1",
+  "4",
+  "4",
+  "5",
+  "1",
+  "2",
+  "5",
+  "4",
+  "1"
+  ]
+  },
+  {
+  "name": "Jacob Deming",
+  "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+  "scores": [
+  "4",
+  "2",
+  "5",
+  "1",
+  "3",
+  "2",
+  "2",
+  "1",
+  "3",
+  "2"
+  ]
+  },
+  {
+  "name": "Jeremiah Scanlon",
+  "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+  "scores": [
+  "5",
+  "2",
+  "2",
+  "2",
+  "4",
+  "1",
+  "3",
+  "2",
+  "5",
+  "5"
+  ]
+  },
+  {
+  "name": "Louis T. Delia",
+  "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+  "scores": [
+  "3",
+  "3",
+  "4",
+  "2",
+  "2",
+  "1",
+  "3",
+  "2",
+  "2",
+  "3"
+  ]
+  },
+  {
+  "name": "Lou Ritter",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "1",
+  "5",
+  "2",
+  "5",
+  "3",
+  "1",
+  "4"
+  ]
+  },
+  {
+  "name": "Jordan Biason",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+  "scores": [
+  "4",
+  "4",
+  "2",
+  "3",
+  "2",
+  "2",
+  "3",
+  "2",
+  "4",
+  "5"
+  ]
+  },
+  {
+  "name": "testing",
+  "photo": "http://jkj.com",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "2",
+  "4",
+  "2",
+  "3",
+  "2",
+  "3",
+  "4"
+  ]
+  },
+  {
+  "name": "testing",
+  "photo": "http://jkj.com",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "2",
+  "4",
+  "2",
+  "3",
+  "2",
+  "3",
+  "4"
+  ]
+  },
+  {
+  "name": "testing",
+  "photo": "http://jkj.com",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "2",
+  "4",
+  "2",
+  "3",
+  "2",
+  "3",
+  "4"
+  ]
+  },
+  {
+  "name": "testing",
+  "photo": "http://jkj.com",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "2",
+  "4",
+  "2",
+  "3",
+  "2",
+  "3",
+  "4"
+  ]
+  },
+  {
+  "name": "jghjkhgjh",
+  "photo": "https://i.imgur.com/asZaNBm.jpg",
+  "scores": [
+  "3",
+  "5",
+  "4",
+  "3",
+  "3",
+  "2",
+  "3",
+  "2",
+  "4",
+  "1"
+  ]
+  },
+  {
+  "name": "jghjkhgjh",
+  "photo": "https://i.imgur.com/asZaNBm.jpg",
+  "scores": [
+  "3",
+  "5",
+  "4",
+  "2",
+  "3",
+  "2",
+  "5",
+  "2",
+  "4",
+  "1"
+  ]
+  },
+  {
+  "name": "Sloth",
+  "photo": "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/Q-Z/photoak-threetoedsloth.ngsversion.1465391618565.png",
+  "scores": [
+  "1",
+  "2",
+  "3",
+  "3",
+  "2",
+  "1",
+  "2",
+  "4",
+  "2",
+  "2"
+  ]
+  },
+  {
+  "name": "Luis",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG",
+  "scores": [
+  "1",
+  "3",
+  "2",
+  "3",
+  "4",
+  "4",
+  "2",
+  "3",
+  "3",
+  "3"
+  ]
+  },
+  {
+  "name": "jeff",
+  "photo": "https://www.google.com/search?q=kermit+picture&rlz=1C1CHBF_enUS806US806&tbm=isch&source=iu&ictx=1&fir=J3uPmeO88-i5tM%253A%252C4dVOohPA5RVYoM%252C_&vet=1&usg=AI4_-kQZWPlWCLoHzSSqdK9BCGmfJFIomw&sa=X&ved=2ahUKEwinm97InIXhAhU-FzQIHdnCBNIQ9QEwAnoECAQQCA#imgrc=J3uPmeO88-i5tM:",
+  "scores": [
+  "5",
+  "2",
+  "5",
+  "1",
+  "3",
+  "5",
+  "5",
+  "1",
+  "5",
+  "5"
+  ]
+  },
+  {
+  "name": "jeff",
+  "photo": "https://www.google.com/search?q=kermit+picture&rlz=1C1CHBF_enUS806US806&tbm=isch&source=iu&ictx=1&fir=J3uPmeO88-i5tM%253A%252C4dVOohPA5RVYoM%252C_&vet=1&usg=AI4_-kQZWPlWCLoHzSSqdK9BCGmfJFIomw&sa=X&ved=2ahUKEwinm97InIXhAhU-FzQIHdnCBNIQ9QEwAnoECAQQCA#imgrc=J3uPmeO88-i5tM:",
+  "scores": [
+  "5",
+  "2",
+  "5",
+  "1",
+  "3",
+  "5",
+  "5",
+  "1",
+  "5",
+  "5"
+  ]
+  },
+  {
+  "name": "jeff",
+  "photo": "https://www.google.com/search?q=kermit+picture&rlz=1C1CHBF_enUS806US806&tbm=isch&source=iu&ictx=1&fir=J3uPmeO88-i5tM%253A%252C4dVOohPA5RVYoM%252C_&vet=1&usg=AI4_-kQZWPlWCLoHzSSqdK9BCGmfJFIomw&sa=X&ved=2ahUKEwinm97InIXhAhU-FzQIHdnCBNIQ9QEwAnoECAQQCA#imgrc=J3uPmeO88-i5tM:",
+  "scores": [
+  "5",
+  "2",
+  "5",
+  "1",
+  "3",
+  "5",
+  "5",
+  "1",
+  "5",
+  "5"
+  ]
+  },
+  {
+  "name": "Johnny",
+  "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGzBqYADlSpIB1mhObj80iB7gojGRAGWloOd87Cr7pkwvhZlf3hQ",
+  "scores": [
+  "2",
+  "3",
+  "4",
+  "1",
+  "4",
+  "3",
+  "3",
+  "4",
+  "1",
+  "3"
+  ]
+  },
+  {
+  "name": "Jamil Weeks",
+  "photo": "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/50928442_10210245587415070_6404991501964673024_n.jpg?_nc_cat=103&_nc_ht=scontent-atl3-1.xx&oh=b1874cfd30fb93b60f2b30f88651da09&oe=5D07D604",
+  "scores": [
+  "5",
+  "2",
+  "1",
+  "3",
+  "1",
+  "3",
+  "5",
+  "1",
+  "3",
+  "3"
+  ]
+  },
+  {
+  "name": "Jumpy Grant",
+  "photo": "https://www.organicfacts.net/wp-content/uploads/corn.jpg",
+  "scores": [
+  "2",
+  "5",
+  "3",
+  "1",
+  "2",
+  "2",
+  "4",
+  "1",
+  "1",
+  "3"
+  ]
+}];
+
+module.exports = friends;
